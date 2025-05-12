5bdee0e902a3272c08d1bf230b38de84889a36f8
willymariino <wg.mariino@gmail.com>
2025-05-12 20:11:56 +0200

    created Header component

---
+++ components/header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <>
+            <header>
+                <h1 className="main-title">il mio blog movies</h1>
+
+            </header>
+        </>
+
+    )
+
+}
+
+export default Header
