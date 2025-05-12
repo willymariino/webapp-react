760ccc4924717891c4847f1d36bbec1ecb8bbd12
willymariino <wg.mariino@gmail.com>
2025-05-13 00:28:20 +0200

    added cards to movie_list

--- pages/movie-list.jsx
+++ pages/movie-list.jsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
+import MovieCard from "../components/movieCard"
 
 function Movie_list() {
     const [movies, setMovies] = useState([])
@@ -15,6 +16,14 @@ function Movie_list() {
     return (
         <>
             <h1>lista dei movies</h1>
+
+            <div className="movie-container">
+                {movies.map(movie => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+
+
+            </div>
         </>
     )
 }
