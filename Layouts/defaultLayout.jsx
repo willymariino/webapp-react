56286a185de6fb77777c258d847cdfde6194d6ea
willymariino <wg.mariino@gmail.com>
2025-05-12 22:10:05 +0200

    added navbar

--- Layouts/defaultLayout.jsx
+++ Layouts/defaultLayout.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/header";
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/navbar";
 
 function DefaultLayout() {
     return (
         <>
             <Header />
+            <Navbar />
             <Outlet />
         </>
     )
