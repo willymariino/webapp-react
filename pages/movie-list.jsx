ca641a2fc683cf780d50b530726cfa55f5bfab71
willymariino <wg.mariino@gmail.com>
2025-05-12 23:08:15 +0200

    feat added axios to Movie-list

--- pages/movie-list.jsx
+++ pages/movie-list.jsx
@@ -1,4 +1,17 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
 function Movie_list() {
+    const [movies, setMovies] = useState([])
+
+    function fetchMovies() {
+        axios.get("http://127.0.0.1:3000/movies")
+            .then((res) => setMovies(res.data.results))
+            .catch((error) => console.error("errore durante il caricamento", error))
+    }
+
+    useEffect(fetchMovies, [])
+
     return (
         <>
             <h1>lista dei movies</h1>
