4482909f3dccd7e739a2410de18f95aa6719b131
willymariino <wg.mariino@gmail.com>
2025-05-13 16:40:46 +0200

    adde axios to movie_detail

--- pages/movie_detail.jsx
+++ pages/movie_detail.jsx
@@ -2,13 +2,25 @@ import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 import axios from "axios"
 
+
 function Movie_detail() {
     const [movies, setMovies] = useState()
     const { id } = useParams()
 
+    function getMovies() {
+        axios.get("http://127.0.0.1:3000/movies/" + id)
+            .then((res) => {
+                setMovies(res.data)
+            })
+            .catch((error) => console.error("errore durante il caricamento", error)
+            )
+    }
+
+    useEffect(getMovies, [id])
+
     return (
         <>
-            <h1>dettaglio movie</h1>
+
         </>
     )
 }
