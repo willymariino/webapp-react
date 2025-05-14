d6061c7a0bc6b2cdcd473ceb65122242ba1172a1
willymariino <wg.mariino@gmail.com>
2025-05-14 22:40:22 +0200

    useState formdata

--- components/addReview.jsx
+++ components/addReview.jsx
@@ -1,33 +1,47 @@
+import { useState } from "react"
 function ReviewForm() {
+
+    const initialValues = {
+        name: "gfh",
+        text: "lorem",
+        vote: 1
+    }
+
+    const [formData, setFormData] = useState(intialValues)
+
+
+
     return (
 
         <>
-            <div className="card container d-flex justify-content-center mt-3 w-50">
-                <div className="card-header">
-                    Scrivi una recensione su questo libro
-                </div>
-                <div className="card-body">
-
-                    <div className="mb-3">
-                        <label htmlFor="reviewer-name" className="form-label">nome recensore</label>
-                        <input type="text" className="form-control" id="reviewer-name" placeholder="inserisci il tuo nome" />
+            <form>
+                <div className="card container d-flex justify-content-center mt-3 w-50">
+                    <div className="card-header">
+                        Scrivi una recensione su questo libro
                     </div>
-                    <div className="mb-3">
-                        <label htmlFor="review-text" className="form-label">testo della recensione</label>
-                        <textarea className="form-control" id="review-text" rows="3"></textarea>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="review-vote" className="form-label">voto:</label>
-                        <input type="number" min={1} max={5} className="form-control" id="review-vote" />
-                    </div>
+                    <div className="card-body">
 
-                    <div className="mb-3">
-                        <button type="submit" className="btn btn-primary">aggiungi</button>
+                        <div className="mb-3">
+                            <label htmlFor="reviewer-name" className="form-label">nome recensore</label>
+                            <input type="text" className="form-control" id="reviewer-name" placeholder="inserisci il tuo nome" value={formData.name} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="review-text" className="form-label">testo della recensione</label>
+                            <textarea className="form-control" id="review-text" rows="3" value={formData.text}></textarea>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="review-vote" className="form-label">voto:</label>
+                            <input type="number" min={1} max={5} className="form-control" id="review-vote" value={formData.vote} />
+                        </div>
+
+                        <div className="mb-3">
+                            <button type="submit" className="btn btn-primary">aggiungi</button>
+                        </div>
+
+
                     </div>
-
-
-                </div>
-            </div >
+                </div >
+            </form>
         </>
     )
 }
