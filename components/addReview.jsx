95df40ac05f62a46f9853a0378f93be98c237d1a
willymariino <wg.mariino@gmail.com>
2025-05-14 20:10:51 +0200

    form

--- components/addReview.jsx
+++ components/addReview.jsx
@@ -7,7 +7,17 @@ function ReviewForm() {
                     Scrivi un arecensione su questo libro
                 </div>
                 <div className="card-body">
-                    form
+
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+
+
                 </div>
             </div >
         </>
