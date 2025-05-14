aa74031d2af0aca1c89c415e33498673a6ec9767
willymariino <wg.mariino@gmail.com>
2025-05-15 00:25:56 +0200

    completed form

--- components/addReview.jsx
+++ components/addReview.jsx
@@ -15,9 +15,8 @@ function ReviewForm() {
         const { name, value } = e.target
         console.log("voglio modificare questo campo")
         let currentValue = value
-        if (name === 'vote') {
-            currentValue = parseInt(value)
-        }
+
+
 
         setFormData((formData) => ({
             ...formData,
@@ -33,27 +32,27 @@ function ReviewForm() {
             <form>
                 <div className="card container d-flex justify-content-center mt-3 w-50">
                     <div className="card-header">
-                        Scrivi una recensione su questo libro
+                        Scrivi una recensione su questo film
                     </div>
                     <div className="card-body">
 
                         <div className="mb-3">
                             <label htmlFor="reviewer-name" className="form-label">nome recensore</label>
-                            <input type="text" className="form-control" id="reviewer-name" placeholder="inserisci il tuo nome" value={formData.name}
+                            <input type="text" className="form-control" id="reviewer-name" placeholder="inserisci il tuo nome" value={formData.name} name="name"
                                 onChange={handleFormData} />
                         </div>
 
 
                         <div className="mb-3">
                             <label htmlFor="review-text" className="form-label">testo della recensione</label>
-                            <textarea className="form-control" id="review-text" rows="3" value={formData.text}
+                            <textarea className="form-control" id="review-text" rows="3" value={formData.text} name="text"
                                 onChange={handleFormData}></textarea>
                         </div>
 
 
                         <div className="mb-3">
                             <label htmlFor="review-vote" className="form-label">voto:</label>
-                            <input type="number" min={1} max={5} className="form-control" id="review-vote" value={formData.vote}
+                            <input type="number" min={1} max={5} className="form-control" id="review-vote" value={formData.vote} name="vote"
                                 onChange={handleFormData} />
                         </div>
 
