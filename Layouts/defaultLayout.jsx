947e69201707a3fbc59bf8cf35aedd65cb589f50
willymariino <wg.mariino@gmail.com>
2025-05-15 15:59:58 +0200

    added loader to layout

--- Layouts/defaultLayout.jsx
+++ Layouts/defaultLayout.jsx
@@ -1,13 +1,21 @@
 import Header from "../components/header";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
+import Loader from "../components/loader";
+import { useContext } from "react";
+import countContext from "../context/globalCotext";
 
 function DefaultLayout() {
+
+    const { isLoading } = useContext(countContext)
+
     return (
         <>
             <Header />
             <Navbar />
+            { }
             <Outlet />
+            {isLoading && <Loader />}
         </>
     )
 }
---
+++ components/loader.jsx
@@ -0,0 +1,8 @@
+function Loader() {
+
+    return (
+        <div> Caricamento in corso... <i class="fa-solid fa-spinner"></i></div>
+    )
+}
+
+export default Loader
